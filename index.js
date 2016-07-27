2c1bf6aa34aa4b6cace744aaea9460aed4968a9c
Nicholas Ginther <GintherN@mccombs.utexas.edu>
2016-07-27 16:14:13 -0500

    init

---
+++ index.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+app.get('/', function(req, res) {
+	res.send('Hello world!');
+});
+
+app.listen(app.get('port'), function() {
+	console.log('listening on ' + app.get('port'));
+});
