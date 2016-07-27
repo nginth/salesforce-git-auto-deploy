f4c65168654c0a34519ecbdfca67f595016776c9
Nicholas Ginther <GintherN@mccombs.utexas.edu>
2016-07-27 16:42:23 -0500

    add body-parser'

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
+
+//middlewares
+app.use(bodyParser.json(););
 
 app.get('/', function(req, res) {
 	res.send('Hello world!');
 });
 
 app.post('/git-push-hook', function(req, res) {
-	res.send(req.body);
+	res.send('heres your body back:\nreq.body');
 });
 
 app.listen(app.get('port'), function() {
