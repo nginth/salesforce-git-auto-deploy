d03ddb77c366feb628d66082ce1e4fa923933e96
Nicholas Ginther <GintherN@mccombs.utexas.edu>
2016-07-27 16:33:53 -0500

    respond to github post push

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ app.get('/', function(req, res) {
 	res.send('Hello world!');
 });
 
+app.post('/git-push-hook', function(req, res) {
+	res.send(req.body);
+});
+
 app.listen(app.get('port'), function() {
 	console.log('listening on ' + app.get('port'));
 });
