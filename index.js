83797fd6d46ecacef07764389bb29b33a042222f
Nick Ginther <nginth@gmail.com>
2016-07-27 22:38:32 -0500

    add security

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
+var crypto = require('crypto');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
@@ -14,7 +15,23 @@ app.get('/', function(req, res) {
 });
 
 app.post('/git-push-hook', function(req, res) {
-	res.send('heres your body back:\n' + JSON.stringify(req.body));
+	var badRequest = false;
+    var reqSignature = req.headers['x-hub-signature'];
+    if (hash) {
+        var envSignature = crypto
+                            .createHmac('sha1', process.env.GHWH_SECRET)
+                            .update(req.rawBody)
+                            .digest('hex');
+        if ('sha1=' + envSignature === reqSignature) {
+            res.send('heres your body back:\n' + JSON.stringify(req.body));
+        }
+        else badRequest = true;    
+    }
+    else badRequest = true;
+    
+    if (badRequest) {
+        res.status('400').send('Bad Request');
+    }
 });
 
 app.listen(app.get('port'), function() {
